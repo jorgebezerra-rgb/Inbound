147229bdcd946d69c4618310e589eab371414050
Jorge0897 <ls.jorge013@gmail.com>
2025-09-12 16:42:16 -0300

    app.py

--- app.py
+++ app.py
@@ -149,7 +149,7 @@ atingimento = (produzido / projecao) * 100 if projecao else 0
 
 if atingimento >= 100:
     st.balloons()
-    # tocar_audio("som_meta_batida.mp3")
+    tocar_audio("som_meta_batida.mp3")
 
 col1, col2, col3 = st.columns(3)
 col1.metric("Produzido (Total)", f"{produzido or 0:,.0f}".replace(",", "."))
@@ -196,3 +196,4 @@ with col_barras:
     else:
 
         st.warning("Não há dados para exibir no gráfico.")
+
